550313c3e9f226241286747eb9a207af571ded5f
HFMarley <hankfoxmarley@gmail.com>
2019-03-14 18:55:59 -0500

    Updates

---
+++ precache-manifest.03655790fcaf2656a1022de83f58f30f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c3a1b5a67c5d677a07d",
+    "url": "/ClickyGame/static/js/runtime~main.2c3a1b5a.js"
+  },
+  {
+    "revision": "24d6c80fd4b7a4d3aaba",
+    "url": "/ClickyGame/static/js/main.24d6c80f.chunk.js"
+  },
+  {
+    "revision": "e40ffb3303712c6cb66e",
+    "url": "/ClickyGame/static/js/2.e40ffb33.chunk.js"
+  },
+  {
+    "revision": "24d6c80fd4b7a4d3aaba",
+    "url": "/ClickyGame/static/css/main.de09ca46.chunk.css"
+  },
+  {
+    "revision": "9b258296ac8e457bd6c0964870252214",
+    "url": "/ClickyGame/index.html"
+  }
+];
